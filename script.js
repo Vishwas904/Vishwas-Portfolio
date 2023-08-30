e57af8b2c676167357a315efac6fb4ac90465f29
Bhambhani Vishwas Sunilbhai <118361500+Vishwas904@users.noreply.github.com>
2023-08-30 18:14:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// Sticky Navbar
+let header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 100);
+
+// Hamburger Menu
+let menuIcon = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+// Typewriter effect
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "UI Designer", "Programmer"],
+  typeSpeed: 80,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Active Section
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
